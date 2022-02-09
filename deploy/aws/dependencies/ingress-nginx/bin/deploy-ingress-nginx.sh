0a34748f810e715d41cdfdc64acc8d143fc2dd3a
Oigen43 <oigen43@icloud.com>
2022-02-09 14:22:31 +0000

    Update deploy-ingress-nginx.sh

--- deploy/aws/dependencies/ingress-nginx/bin/deploy-ingress-nginx.sh
+++ deploy/aws/dependencies/ingress-nginx/bin/deploy-ingress-nginx.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 cd "$( dirname "${BASH_SOURCE[0]}" )"/../
 
+helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
+helm repo update
+
 helm upgrade --install ingress-nginx ingress-nginx \
   --repo https://kubernetes.github.io/ingress-nginx \
   --namespace ingress-nginx --create-namespace \
