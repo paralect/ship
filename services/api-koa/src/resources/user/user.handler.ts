f3114b2d2fe3b6ccf36f314a079000d09a05c5dc
Oigen43 <oigen43@icloud.com>
2022-06-21 02:34:54 +0300

    koa-api: add onUpdated example

--- services/api-koa/src/resources/user/user.handler.ts
+++ services/api-koa/src/resources/user/user.handler.ts
@@ -1,11 +1,18 @@
 import { eventBus, InMemoryEvent } from '@paralect/node-mongo';
 
 import ioEmitter from 'io-emitter';
+import { DATABASE_DOCUMENTS } from 'app.constants';
 
 import { User } from './user.types';
 
-eventBus.on('users.updated', (data: InMemoryEvent<User>) => {
+const { USERS } = DATABASE_DOCUMENTS;
+
+eventBus.on(`${USERS}.updated`, (data: InMemoryEvent<User>) => {
   const user = data.doc;
 
   ioEmitter.publishToUser(user._id, 'user:updated', user);
+});
+
+eventBus.onUpdated(USERS, ['fullname'], (data: InMemoryEvent<User>) => {
+
 });
