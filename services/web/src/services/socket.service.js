06eea0cff4ed2a0c1e5337d16b7dc619bec1bb7b
Alexey Monich <a.monich@paralect.com>
2022-08-02 17:05:14 +0300

    add socket.signalr.service.js

--- services/web/src/services/socket.service.js
+++ services/web/src/services/socket.service.js
@@ -2,7 +2,7 @@ import io from 'socket.io-client';
 
 import config from 'config';
 
-const socket = io(config.apiUrl, {
+const socket = io(config.wsUrl, {
   transports: ['websocket'],
   autoConnect: false,
 });
