66cdf9f977eab57b1fd02e8cc76d6fac0c5c1b18
Alexey Monich <a.monich@paralect.com>
2022-08-02 17:03:21 +0300

    add a OnConnectedAsync method to SignalR Hub

--- services/api-dotnet/src/app/SignalR/Hubs/UserHub.cs
+++ services/api-dotnet/src/app/SignalR/Hubs/UserHub.cs
@@ -4,6 +4,14 @@ namespace SignalR.Hubs
 {
     public class UserHub : Hub
     {
+        // This method is required to unify interaction with the client application.
+        // The client can use SignalR or Socket.IO as event-based communication library.
+        // In Socket.IO after a connection is established, the client expects to receive a "connect" message.
+        public override async Task OnConnectedAsync()
+        {
+            await Clients.Caller.SendAsync("connect");
+        }
+
         public async Task Subscribe(string groupName)
         {
             if (HasAccessToRoom(groupName))
