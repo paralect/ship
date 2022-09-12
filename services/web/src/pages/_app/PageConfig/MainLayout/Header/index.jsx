21033fd8aef0ac0ac439ab7e3e9d78fa99e935ab
igor <ionovitsky@gmail.com>
2022-09-12 14:56:52 +0300

    feat: Add updated retool json file

--- services/web/src/pages/_app/PageConfig/MainLayout/Header/index.jsx
+++ services/web/src/pages/_app/PageConfig/MainLayout/Header/index.jsx
@@ -7,33 +7,39 @@ import {
 import { Link } from 'components';
 import { LogoImage } from 'public/images';
 
+import { userApi } from 'resources/user';
+
 import AddMembersModal from './components/AddMembersModal';
 import UserMenu from './components/UserMenu';
 import ShadowLoginBanner from './components/ShadowLoginBanner';
 
-const Header = () => (
-  <LayoutHeader
-    component="header"
-  >
-    <Container
-      sx={(theme) => ({
-        minHeight: '72px',
-        padding: '0 32px',
-        backgroundColor: theme.black,
-        display: 'flex',
-        alignItems: 'center',
-        flex: '1 1 auto',
-      })}
-      fluid
+const Header = () => {
+  const { data: user } = userApi.useGetCurrent();
+
+  return (
+    <LayoutHeader
+      component="header"
     >
-      <Link type="router" href={routes.path.home} withoutUnderline>
-        <LogoImage />
-      </Link>
-      <AddMembersModal />
-      <UserMenu />
-    </Container>
-    <ShadowLoginBanner />
-  </LayoutHeader>
-);
+      <Container
+        sx={(theme) => ({
+          minHeight: '72px',
+          padding: '0 32px',
+          backgroundColor: theme.black,
+          display: 'flex',
+          alignItems: 'center',
+          flex: '1 1 auto',
+        })}
+        fluid
+      >
+        <Link type="router" href={routes.path.home} withoutUnderline>
+          <LogoImage />
+        </Link>
+        <AddMembersModal />
+        <UserMenu />
+      </Container>
+      {user.isShadow && <ShadowLoginBanner />}
+    </LayoutHeader>
+  );
+};
 
 export default memo(Header);
