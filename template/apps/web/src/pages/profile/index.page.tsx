883bdf87e66bfe211a4979da3f888cc0e6f2bb7e
casder <casderiopus@gmail.com>
2022-11-11 13:49:40 +0300

    Update profile page

--- template/apps/web/src/pages/profile/index.page.tsx
+++ template/apps/web/src/pages/profile/index.page.tsx
@@ -19,6 +19,8 @@ import { accountApi } from 'resources/account';
 import PhotoUpload from './components/file-upload';
 
 const schema = z.object({
+  firstName: z.string().min(1, 'Please enter First name').max(100),
+  lastName: z.string().min(1, 'Please enter Last name').max(100),
   password: z.string().regex(
     /^$|^(?=.*[a-z])(?=.*\d)[A-Za-z\d\W]{6,}$/g,
     'The password must contain 6 or more characters with at least one letter (a-z) and one number (0-9).',
@@ -39,6 +41,10 @@ const Profile: NextPage = () => {
     formState: { errors },
   } = useForm<UpdateParams>({
     resolver: zodResolver(schema),
+    defaultValues: {
+      firstName: account?.firstName,
+      lastName: account?.lastName,
+    },
   });
 
   const {
@@ -51,7 +57,7 @@ const Profile: NextPage = () => {
       queryClient.setQueryData(['currentUser'], data);
       showNotification({
         title: 'Success',
-        message: 'Your password have been successfully updated.',
+        message: 'Your profile has been successfully updated.',
         color: 'green',
       });
     },
@@ -64,29 +70,49 @@ const Profile: NextPage = () => {
         <title>Profile</title>
       </Head>
       <Stack
-        sx={{ width: '328px', margin: 'auto', paddingTop: '48px' }}
-        spacing="xl"
+        sx={{ width: '408px', margin: 'auto', paddingTop: '48px' }}
+        spacing={32}
       >
         <Title order={1}>Profile</Title>
         <PhotoUpload />
         <form
-          style={{ display: 'flex', flexDirection: 'column', gap: 'inherit' }}
+          style={{ display: 'flex', flexDirection: 'column', gap: '34px' }}
           onSubmit={handleSubmit(onSubmit)}
         >
-          <TextInput
-            label="Email Address"
-            defaultValue={account?.email}
-            disabled
-          />
-          <PasswordInput
-            {...register('password')}
-            label="Password"
-            placeholder="Your password"
-            labelProps={{
-              'data-invalid': !!errors.password,
-            }}
-            error={errors.password?.message}
-          />
+          <Stack spacing={20}>
+            <TextInput
+              {...register('firstName')}
+              label="First Name"
+              placeholder="First Name"
+              labelProps={{
+                'data-invalid': !!errors.firstName,
+              }}
+              error={errors.firstName?.message}
+            />
+            <TextInput
+              {...register('lastName')}
+              label="Last Name"
+              placeholder="Last Name"
+              labelProps={{
+                'data-invalid': !!errors.lastName,
+              }}
+              error={errors.lastName?.message}
+            />
+            <TextInput
+              label="Email Address"
+              defaultValue={account?.email}
+              disabled
+            />
+            <PasswordInput
+              {...register('password')}
+              label="Password"
+              placeholder="Enter password"
+              labelProps={{
+                'data-invalid': !!errors.password,
+              }}
+              error={errors.password?.message}
+            />
+          </Stack>
           <Button
             type="submit"
             loading={isUpdateLoading}
