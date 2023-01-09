52d851c7097fc157d7c9bbbbfd1e263ac0af860f
Anton Tsapliuk <tsapa44@gmail.com>
2023-01-09 15:23:27 +0300

    scheduler: add example how to use schedule (#199)

--- template/apps/api/src/scheduler/cron/index.ts
+++ template/apps/api/src/scheduler/cron/index.ts
@@ -7,4 +7,8 @@ schedule.scheduleJob('* * * * *', () => {
   eventEmitter.emit('cron:every-minute');
 });
 
+schedule.scheduleJob('0 * * * *', () => {
+  eventEmitter.emit('cron:every-hour');
+});
+
 export default eventEmitter;
