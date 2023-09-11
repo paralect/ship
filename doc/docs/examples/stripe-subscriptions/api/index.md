1ccdfcf38882a860942327ed8c2f1cbe2464ba67
Evgeny <fruneen@gmail.com>
2023-09-11 13:32:39 +0300

    feat: remove docusarus documenation

--- doc/docs/examples/stripe-subscriptions/api/index.md
+++
@@ -1,3 +0,0 @@
-# API
-
-This section contains list of stripe api calls, used in the application with brief description of each.
--- doc/src/pages/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import {Redirect} from '@docusaurus/router';
-import Head from "@docusaurus/Head";
-
-const Home = () => {
-    return <Redirect to="/docs/intro" />;
-};
-
-export default Home;
