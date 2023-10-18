7d2d90af23cb1d9bb19cbefc7d279f2f77fa9f02
Vlad Solovey <connectwithvlad@gmail.com>
2023-10-18 11:03:38 +0300

    feat: update UnauthorizedLayout in PageConfig

--- template/apps/web/src/pages/_app/PageConfig/UnauthorizedLayout/index.tsx
+++ template/apps/web/src/pages/_app/PageConfig/UnauthorizedLayout/index.tsx
@@ -1,41 +1,31 @@
 import { FC, ReactElement } from 'react';
 
-import { SimpleGrid, Image, MediaQuery } from '@mantine/core';
+import { SimpleGrid, Image } from '@mantine/core';
 
-import { useStyles } from './styles';
+import classes from './UnauthorizedLayout.module.css';
 
 interface UnauthorizedLayoutProps {
   children: ReactElement;
 }
 
-const UnauthorizedLayout: FC<UnauthorizedLayoutProps> = ({ children }) => {
-  const { classes } = useStyles();
+const UnauthorizedLayout: FC<UnauthorizedLayoutProps> = ({ children }) => (
+  <SimpleGrid
+    cols={{ base: 1, sm: 2 }}
+    spacing="sm"
+  >
+    <Image
+      className={classes.image}
+      alt="App Info"
+      src="/images/ship.svg"
+      height="100vh"
+    />
 
-  return (
-    <SimpleGrid
-      cols={2}
-      breakpoints={[
-        { maxWidth: 'sm', cols: 1, spacing: 'sm' },
-      ]}
-    >
-      <MediaQuery
-        smallerThan="sm"
-        styles={{ display: 'none' }}
-      >
-        <Image
-          alt="App Info"
-          src="/images/ship.svg"
-          height="100vh"
-        />
-      </MediaQuery>
-
-      <div className={classes.wrapper}>
-        <main className={classes.content}>
-          {children}
-        </main>
-      </div>
-    </SimpleGrid>
-  );
-};
+    <div className={classes.wrapper}>
+      <main className={classes.content}>
+        {children}
+      </main>
+    </div>
+  </SimpleGrid>
+);
 
 export default UnauthorizedLayout;
