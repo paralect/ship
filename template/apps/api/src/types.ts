e4245f1398dc6d836dca22927cb8420e9f6222be
ilyevskii <ilya.andreevskii2004@gmail.com>
2023-12-01 20:27:53 +0300

    fix: fix issue connected with using enums in web app

--- template/apps/api/src/types.ts
+++ template/apps/api/src/types.ts
@@ -3,25 +3,27 @@ import Router from '@koa/router';
 
 import { Template } from 'mailer';
 
-import { User } from './user.types';
+import { User } from 'app-types';
+
+export * from 'app-types';
 
 export type AppKoaContextState = {
-  user: User;
-  accessToken: string;
-  isShadow: boolean | null;
+    user: User;
+    accessToken: string;
+    isShadow: boolean | null;
 };
 
 export type CustomErrors = {
-  [name: string]: string;
+    [name: string]: string;
 };
 
 export interface AppKoaContext<T = unknown, R = unknown> extends ParameterizedContext<AppKoaContextState> {
-  request: Request & R;
-  validatedData: T;
-  throwError: (message: string) => never;
-  assertError: (condition: unknown, message: string) => asserts condition;
-  throwClientError: (errors: CustomErrors, status?: number) => never;
-  assertClientError: (condition: unknown, errors: CustomErrors, status?: number) => asserts condition;
+    request: Request & R;
+    validatedData: T;
+    throwError: (message: string) => never;
+    assertError: (condition: unknown, message: string) => asserts condition;
+    throwClientError: (errors: CustomErrors, status?: number) => never;
+    assertClientError: (condition: unknown, errors: CustomErrors, status?: number) => asserts condition;
 }
 
 export class AppRouter extends Router<AppKoaContextState, AppKoaContext> {}
@@ -31,7 +33,7 @@ export class AppKoa extends Koa<AppKoaContextState, AppKoaContext> {}
 export type AppRouterMiddleware = Router.Middleware<AppKoaContextState, AppKoaContext>;
 
 export type ValidationErrors = {
-  [name: string]: string[] | string;
+    [name: string]: string[] | string;
 };
 
 export { Next, Template };
