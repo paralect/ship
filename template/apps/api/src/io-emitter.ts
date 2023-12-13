65fc6866b66ebdb2806208144499bf6ebe8d651c
Evgeny <fruneen@gmail.com>
2023-12-13 12:57:37 +0300

    feat: introduce new logic for handling redis errors

--- template/apps/api/src/io-emitter.ts
+++ template/apps/api/src/io-emitter.ts
@@ -1,30 +1,32 @@
-import { createClient } from 'redis';
 import { Emitter } from '@socket.io/redis-emitter';
 
-import config from 'config';
+import redisClient from 'redis-client';
 import logger from 'logger';
 
-let emitter: Emitter | null = null;
-const redisClient = createClient({ url: config.REDIS_URI });
+import config from './config';
 
-redisClient.on('error', err => {
-  logger.error(`ioEmitter() => Redis error: ${err.stack || err}`);
-  throw err;
-});
+let emitter: Emitter | null = null;
 
 const publish = (roomId: string | string[], eventName: string, data: unknown) => {
   if (emitter === null) {
-    throw new Error('ioEmitter is not initialized.');
+    if (config.REDIS_ERRORS_POLICY === 'throw') {
+      throw new Error('ioEmitter is not initialized.');
+    } else {
+      logger.debug('ioEmitter is not initialized.');
+      return;
+    }
   }
 
   logger.debug(`published io event [${eventName}] to ${roomId}, the data is: ${JSON.stringify(data)}`);
+
   emitter.to(roomId).emit(eventName, data);
 };
 
 const initClient = async () => {
-  await redisClient.connect();
+  const subClient = redisClient.duplicate();
+  await subClient.connect();
 
-  emitter = new Emitter(redisClient);
+  emitter = new Emitter(subClient);
 };
 
 const getUserRoomId = (userId: string) => `user-${userId}`;
--- template/apps/api/src/redis-client.ts
+++ template/apps/api/src/redis-client.ts
@@ -3,11 +3,28 @@ import { createClient } from 'redis';
 import config from 'config';
 import logger from 'logger';
 
-const client = createClient({ url: config.REDIS_URI });
+const client = createClient({
+  url: config.REDIS_URI,
+  pingInterval: 1_000,
+  socket: {
+    connectTimeout: 30_000,
+    reconnectStrategy: (retries) => {
+      const maxDelay = 5_000;
+      const baseDelay = 50;
+
+      return Math.min(baseDelay * Math.pow(2, retries), maxDelay);
+    },
+  },
+});
 
 client.on('error', err => {
-  logger.error(`redisClient => Redis error: ${err.stack || err}`);
-  throw err;
+  const errorMessage = `redisClient => Redis error: ${err.stack || err}`;
+
+  if (config.REDIS_ERRORS_POLICY === 'throw') {
+    throw Error(errorMessage);
+  } else {
+    logger.error(errorMessage);
+  }
 });
 
 export default client;
