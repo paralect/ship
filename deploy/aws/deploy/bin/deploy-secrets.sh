79b9273608526c0a067df597bb4535973328418e
Darya <daryasamsonowicz@gmail.com>
2024-01-08 15:47:48 +0300

    feat: update secret name and solve namespace error

--- deploy/aws/deploy/bin/deploy-secrets.sh
+++ deploy/aws/deploy/bin/deploy-secrets.sh
@@ -1,33 +1,24 @@
 #!/bin/bash
 
-read -p "What secret name? " secret_name
-
-read -p "What stage? (staging, production) " stage
-
-echo "Set up necessary environment variables"
-read -p "Write MONGO_URI "  mongo_uri
-
-read -p "Write MONGO_DB_NAME " mongo_db_name
-
-read -p "Write API_URL " api_url
-
-read -p "Write WEB_URL " web_url
-
-declare -a additional_envs
+declare -a envs
 
 while true; do
+    read -p "What variable name? " var_name
+    read -p "What variable value? " var_value
+
+    envs+=" --from-literal=$var_name=$var_value"
+
     read -p "Do you need to write more environment variables? (y/n) " response
         if [[ $response == "n" ]]; then
             break
-        else
-          read -p "What env name? " var_name
-          read -p "What env value? " var_value
-
-          additional_envs+=" --from-literal=$var_name=$var_value"
         fi
 done
 
-kubectl create namespace $stage
+namespaces=$(kubectl get namespaces)
 
-kubectl create -n $stage secret generic $secret_name --from-literal=MONGO_URI="$mongo_uri" --from-literal=MONGO_DB_NAME=$mongo_db_name --from-literal=API_URL=$api_url --from-literal=WEB_URL=$web_url $additional_envs
+if ! grep -q "$stage" <<< "$namespaces"; then
+    kubectl create namespace $stage
+fi
+
+kubectl create -n $stage secret generic "api-$stage-secret" $envs
 
