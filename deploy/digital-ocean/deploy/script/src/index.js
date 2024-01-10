ed643698e7d926bb09730a3607750e71176ca064
Darya <daryasamsonowicz@gmail.com>
2024-01-10 12:54:15 +0300

    feat: change DO kubernetes deploy instruction

--- deploy/digital-ocean/deploy/script/src/index.js
+++ deploy/digital-ocean/deploy/script/src/index.js
@@ -7,12 +7,12 @@ const { execCommand } = require('./util');
 const askServiceToDeploy = async () => {
   const utils = ['migrator', 'scheduler'];
   const choices = Object.keys(config.deploy).filter((c) => !utils.includes(c));
-  
+
   let serviceToDeploy;
-  
+
   if (config.service) {
     serviceToDeploy = config.service;
-    
+
     if (!choices.includes(serviceToDeploy)) {
       throw new Error(`Wrong service specified to deploy [${serviceToDeploy}]. Aborting`);
     }
@@ -22,13 +22,13 @@ const askServiceToDeploy = async () => {
       message: 'What service to deploy?',
       choices,
     });
-    
+
     serviceToDeploy = await list.run();
   }
-  
+
   const serviceConfig = config.deploy[serviceToDeploy];
   serviceConfig.name = serviceToDeploy;
-  
+
   return serviceConfig;
 };
 
@@ -44,13 +44,13 @@ const buildAndPushImage = async ({ dockerFilePath, dockerRepo, dockerContextDir,
 
 const pushToKubernetes = async ({ imageTag, appName, deployConfig }) => {
   const deployDir = `${config.rootDir}/deploy/app/${deployConfig.folder}`;
-  
+
   if (config.kubeConfig && !fs.existsSync(`${config.home}/.kube/config`)) {
     console.log('Creating kubeconfig');
     fs.mkdirSync(`${config.home}/.kube`);
     fs.writeFileSync(`${config.home}/.kube/config`, config.kubeConfig);
   }
-  
+
   await execCommand(`
     helm upgrade --install apps-${config.environment}-${appName} ${deployDir} \
       --namespace ${config.namespace} --create-namespace \
@@ -58,6 +58,7 @@ const pushToKubernetes = async ({ imageTag, appName, deployConfig }) => {
       --set imagesVersion=${imageTag} \
       --set nodePool=${config.nodePool} \
       --set containerRegistry=${config.dockerRegistry.name} \
+      --set environment=${config.environment} \
       -f ${deployDir}/${config.environment}.yaml \
       --timeout 35m \
   `);
@@ -68,16 +69,16 @@ const deploy = async () => {
     await execCommand(`docker login --username ${config.dockerRegistry.username} --password ${config.dockerRegistry.password} registry.digitalocean.com`);
   }
   const deployConfig = await askServiceToDeploy();
-  
+
   let imageTag = config.dockerRegistry.imageTag;
-  
+
   if (!imageTag) {
     const { stdout: branch } = await execCommand('git rev-parse --abbrev-ref HEAD', { stdio: 'pipe' });
     const { stdout: commitSHA } = await execCommand('git rev-parse HEAD', { stdio: 'pipe' });
-    
+
     imageTag = `${branch}.${commitSHA}`;
   }
-  
+
   if (deployConfig.name === 'api') {
     // push migrator image to registry
     await buildAndPushImage({
@@ -85,28 +86,28 @@ const deploy = async () => {
       imageTag: `${config.deploy.migrator.dockerRepo}:${imageTag}`,
       environment: config.environment
     });
-    
+
     // push api image to registry
     await buildAndPushImage({
       ...deployConfig,
       imageTag: `${deployConfig.dockerRepo}:${imageTag}`,
       environment: config.environment
     });
-    
+
     // deploy api to kubernetes and deploy migrator through helm hooks
     await pushToKubernetes({
       imageTag,
       appName: 'api',
       deployConfig
     });
-    
+
     // push scheduler image to registry
     await buildAndPushImage({
       ...config.deploy.scheduler,
       imageTag: `${config.deploy.scheduler.dockerRepo}:${imageTag}`,
       environment: config.environment
     });
-    
+
     // deploy scheduler to kubernetes
     await pushToKubernetes({
       imageTag,
@@ -114,7 +115,7 @@ const deploy = async () => {
       deployConfig: config.deploy.scheduler
     });
   }
-  
+
   if (deployConfig.name === 'web') {
     // push web image to registry
     await buildAndPushImage({
@@ -122,7 +123,7 @@ const deploy = async () => {
       imageTag: `${deployConfig.dockerRepo}:${imageTag}`,
       environment: config.environment
     });
-    
+
     // deploy web to kubernetes
     await pushToKubernetes({
       imageTag,
