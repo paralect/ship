96c3a10e22294df8408f52db25744776f584f627
fruneen <fruneen@gmail.com>
2024-02-29 15:16:42 +0300

    feat: add src prop to user menu avatar

--- template/apps/web/src/pages/_app/PageConfig/MainLayout/Header/components/MenuToggle/index.tsx
+++ template/apps/web/src/pages/_app/PageConfig/MainLayout/Header/components/MenuToggle/index.tsx
@@ -12,7 +12,7 @@ const MenuToggle = forwardRef<HTMLButtonElement>((props, ref) => {
 
   return (
     <UnstyledButton ref={ref} {...props}>
-      <Avatar color={primaryColor} radius="xl">
+      <Avatar src={account.avatarUrl} color={primaryColor} radius="xl">
         {account.firstName.charAt(0)}
         {account.lastName.charAt(0)}
       </Avatar>
