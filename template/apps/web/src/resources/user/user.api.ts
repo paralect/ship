79c30247f24492f88162369a16826b612505a12a
fruneen <fruneen@gmail.com>
2024-03-04 13:37:49 +0300

    feat: add improvements to list endpoint

--- template/apps/web/src/resources/user/user.api.ts
+++ template/apps/web/src/resources/user/user.api.ts
@@ -1,18 +1,10 @@
 import { useQuery } from '@tanstack/react-query';
 
-import { User } from 'types';
+import { ListResult, User } from 'types';
 
 import { apiService } from 'services';
 
-export function useList<T>(params: T) {
-  interface UserListResponse {
-    count: number;
-    items: User[];
-    totalPages: number;
-  }
-
-  return useQuery<UserListResponse>({
-    queryKey: ['users', params],
-    queryFn: () => apiService.get('/users', params),
-  });
-}
+export const useList = <T>(params: T) => useQuery<ListResult<User>>({
+  queryKey: ['users', params],
+  queryFn: () => apiService.get('/users', params),
+});
--- template/packages/schemas/src/index.ts
+++ template/packages/schemas/src/index.ts
@@ -1,2 +1,4 @@
+export * from './pagination.schema';
+
 export * from './token.schema';
 export * from './user.schema';
