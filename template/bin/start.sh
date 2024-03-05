73eb8dc6b61f487798444f2fa11e24c97fce9725
Evgeny Chaban <fruneen@gmail.com>
2024-03-05 17:45:12 +0300

    Implement auto generation MongoDB keyfile (#284)
    * feat: move mongo volume to .dev folder
    
    * feat: add auto generation mongo keyfile
    
    * feat: add .dev folder to .gitignore
    
    * feat: delete mongo keyfile from api folder
    
    * feat: move mongodb config to docker volumes

--- template/bin/start.sh
+++ template/bin/start.sh
@@ -1,4 +1,19 @@
 #!/bin/sh
+
+KEYFILE_DIR=".dev/mongo/config"
+KEYFILE="$KEYFILE_DIR/keyfile"
+
+if [ ! -f "$KEYFILE" ]; then
+    echo "MongoDB keyfile does not exist. Generating now..."
+
+    mkdir -p "$KEYFILE_DIR"
+
+    openssl rand -base64 756 > "$KEYFILE"
+    chmod 400 "$KEYFILE"
+
+    echo "MongoDB keyfile generated."
+fi
+
 export DOCKER_CLIENT_TIMEOUT=600
 export COMPOSE_HTTP_TIMEOUT=600
 Green='\033[0;32m'
