472c34ccd1c66769f93000cfb556648bd87d78a3
ilyevskii <ilya.andreevskii2004@gmail.com>
2024-07-09 16:08:44 +0300

    refactor: flip condition

--- template/apps/api/src/utils/object.util.ts
+++ template/apps/api/src/utils/object.util.ts
@@ -1,9 +1,9 @@
 import _ from 'lodash';
 
 const flattenObject = <T>(obj: object, path: string[] = []): Record<string, T[keyof T]> =>
-  !_.isObject(obj)
-    ? { [path.join('.')]: obj }
-    : Object.entries(obj).reduce((cur, [key, value]) => _.merge(cur, flattenObject<T>(value, [...path, key])), {});
+  _.isObject(obj)
+    ? Object.entries(obj).reduce((cur, [key, value]) => _.merge(cur, flattenObject<T>(value, [...path, key])), {})
+    : { [path.join('.')]: obj };
 
 export default {
   flattenObject,
