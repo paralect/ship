61d3ddd84bb077b159a7d9a14b87255bdb21279a
Evgeny Chaban <fruneen@gmail.com>
2024-07-10 15:14:51 +0300

    Storybook: add basic example of usage in UI kit, upgrade version (#309)
    * feat: add example with basic usage of storybook
    
    * fix: change title prefix on story block

--- template/apps/web/.storybook/preview.tsx
+++ template/apps/web/.storybook/preview.tsx
@@ -1,6 +1,6 @@
 import '@mantine/core/styles.css';
 
-import React, { useEffect } from 'react';
+import { ReactNode, useEffect } from 'react';
 import { addons } from '@storybook/preview-api';
 import { DARK_MODE_EVENT_NAME } from 'storybook-dark-mode';
 import { MantineProvider, useMantineColorScheme } from '@mantine/core';
@@ -9,17 +9,15 @@ import theme from '../src/theme';
 
 const channel = addons.getChannel();
 
-const ColorSchemeWrapper = ({ children }: { children: React.ReactNode }) => {
+const ColorSchemeWrapper = ({ children }: { children: ReactNode }) => {
   const { setColorScheme } = useMantineColorScheme();
   const handleColorScheme = (value: boolean) => setColorScheme(value ? 'dark' : 'light');
 
   useEffect(() => {
     channel.on(DARK_MODE_EVENT_NAME, handleColorScheme);
     return () => channel.off(DARK_MODE_EVENT_NAME, handleColorScheme);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [channel]);
 
-  // eslint-disable-next-line react/jsx-no-useless-fragment
   return <>{children}</>;
 };
 
