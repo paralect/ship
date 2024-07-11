6fa9cb7b72a9507fe649cd27cace4c54f52577b4
Ilya Andreevskii <95957223+ilyevskii@users.noreply.github.com>
2024-07-11 16:03:42 +0300

    fix: fix redirect on the 404 page (#311)

--- template/apps/web/src/pages/404/index.page.tsx
+++ template/apps/web/src/pages/404/index.page.tsx
@@ -1,26 +1,30 @@
 import React, { NextPage } from 'next';
 import Head from 'next/head';
-import router from 'next/router';
+import { useRouter } from 'next/router';
 import { Button, Stack, Text, Title } from '@mantine/core';
 
 import { RoutePath } from 'routes';
 
-const NotFound: NextPage = () => (
-  <>
-    <Head>
-      <title>Page not found</title>
-    </Head>
+const NotFound: NextPage = () => {
+  const { replace } = useRouter();
 
-    <Stack h="100vh" w={328} justify="center" m="auto">
-      <Title order={2}>Oops! The page is not found.</Title>
+  return (
+    <>
+      <Head>
+        <title>Page not found</title>
+      </Head>
 
-      <Text mx={0} mt={20} mb={24} c="gray.6">
-        The page you are looking for may have been removed, or the link you followed may be broken.
-      </Text>
+      <Stack h="100vh" w={328} justify="center" m="auto">
+        <Title order={2}>Oops! The page is not found.</Title>
 
-      <Button onClick={() => router.push(RoutePath.Home)}>Go to homepage</Button>
-    </Stack>
-  </>
-);
+        <Text mx={0} mt={20} mb={24} c="gray.6">
+          The page you are looking for may have been removed, or the link you followed may be broken.
+        </Text>
+
+        <Button onClick={() => replace(RoutePath.Home)}>Go to homepage</Button>
+      </Stack>
+    </>
+  );
+};
 
 export default NotFound;
