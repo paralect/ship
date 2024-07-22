8e125856f38c2e34dccc1a63c976ebcf348813aa
yauhenipliuta <enhorwp@gmail.com>
2024-07-22 19:10:01 +0200

    [Web]add: DynamicMessage component

---
+++ template/apps/web/src/pages/open-ai/components/DynamicMessage/index.tsx
@@ -0,0 +1,17 @@
+import React, { memo } from 'react';
+
+import CustomPre from '../CustomPre/index';
+
+interface DynamicMessageProps {
+  streamParts: string[];
+}
+
+const DynamicMessage: React.FC<DynamicMessageProps> = ({ streamParts }) => (
+  <CustomPre>
+    {streamParts.map((part) => (
+      <React.Fragment key={crypto.randomUUID()}>{part}</React.Fragment>
+    ))}
+  </CustomPre>
+);
+
+export default memo(DynamicMessage);
