3d6baf4617b075e7e211fbd20271f33f9aa93e54
fruneen <fruneen@gmail.com>
2024-08-19 15:36:11 +0300

    feat: implement copyng of .env.example file into .env if .env file does not exist

--- template/bin/start.sh
+++ template/bin/start.sh
@@ -15,6 +15,14 @@ if [ ! -f "$KEYFILE" ]; then
 fi
 
 API_ENV_FILE="apps/api/.env"
+API_ENV_EXAMPLE_FILE="apps/api/.env.example"
+
+if [ ! -f "$API_ENV_FILE" ]; then
+    echo "$API_ENV_FILE does not exist. Copying from $API_ENV_EXAMPLE_FILE..."
+    cp "$API_ENV_EXAMPLE_FILE" "$API_ENV_FILE"
+    echo "$API_ENV_FILE created from $API_ENV_EXAMPLE_FILE."
+fi
+
 if ! grep -q "^JWT_SECRET=" "$API_ENV_FILE"; then
     echo "JWT_SECRET does not exist in $API_ENV_FILE. Generating now..."
 
