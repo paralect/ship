7221d09a8b477f05106583b7001752a5551eb6e5
fruneen <fruneen@gmail.com>
2025-07-21 15:36:14 +0300

    feat: migrate packages to eslint 9 config

---
+++ template/packages/mailer/eslint.config.js
@@ -0,0 +1,3 @@
+import next from 'eslint-config/next.js';
+
+export default next;
--- template/packages/mailer/src/utils.tsx
+++ template/packages/mailer/src/utils.tsx
@@ -1,9 +1,7 @@
 import React, { FC } from 'react';
-import { Options, renderAsync } from '@react-email/render';
+import { Options, render } from '@react-email/render';
 
 import { EmailComponent, Template, TemplateProps } from './template';
-
-export * from './template';
 
 export interface RenderEmailHtmlProps<T extends Template> {
   template: T;
@@ -14,5 +12,5 @@ export interface RenderEmailHtmlProps<T extends Template> {
 export const renderEmailHtml = async <T extends Template>({ template, params, options }: RenderEmailHtmlProps<T>) => {
   const Component = EmailComponent[template] as FC<TemplateProps[T]>;
 
-  return renderAsync(<Component {...params} />, options);
+  return render(<Component {...params} />, options);
 };
