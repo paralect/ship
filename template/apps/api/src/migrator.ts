9e3b8f63a0257170e2719812213ed0365a6438a1
stasik1404 <s.kanashuk@golance.com>
2025-08-21 17:08:30 +0300

    fix: Rewritten all modules to ESM

--- template/apps/api/src/migrator.ts
+++ template/apps/api/src/migrator.ts
@@ -1,11 +1,4 @@
-/* eslint-disable simple-import-sort/imports, import/first */
-// allows to require modules relative to /src folder
-// for example: require('lib/mongo/idGenerator')
-// all options can be found here: https://gist.github.com/branneman/8048520
-import moduleAlias from 'module-alias'; // read aliases from package json
-
-moduleAlias.addPath(__dirname);
-moduleAlias();
+/* eslint-disable simple-import-sort/imports */
 
 import 'dotenv/config';
 
