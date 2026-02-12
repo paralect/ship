c242e2f8398f120d725f043560f7aeb3e400aa01
Xenia Levchenko <xenia@admoji.com>
2026-02-12 19:20:18 +0300

    refactor

--- template/apps/web-shadcn/src/pages/_document.page.tsx
+++ template/apps/web-shadcn/src/pages/_document.page.tsx
@@ -1,17 +1,3 @@
-import { Head, Html, Main, NextScript } from 'next/document';
-
-const Document = () => (
-  <Html lang="en">
-    <Head>
-      <link rel="icon" href="/favicon.ico" />
-    </Head>
-
-    <body className="min-h-screen bg-background font-sans antialiased">
-      <Main />
-
-      <NextScript />
-    </body>
-  </Html>
-);
+import Document from 'pages/_document';
 
 export default Document;
