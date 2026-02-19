4d1fffdce8f46cf22fed8fd99747e17639dde0b3
Xenia Levchenko <xenia@admoji.com>
2026-02-19 19:54:26 +0300

    add navbar to layout

--- template/apps/web/src/pages/home/components/chat/index.ts
+++
@@ -1,6 +0,0 @@
-export { default as ChatBox } from './ChatBox';
-export { default as ChatInput } from './ChatInput';
-export { default as ChatMessage } from './ChatMessage';
-export type { Message } from './ChatMessage';
-export { default as ChatSidebar } from './ChatSidebar';
-export type { Chat } from './ChatSidebar';
