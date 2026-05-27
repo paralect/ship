80fce1b7c3e36fb755d375f8ed02a04f25641478
Alexey Kasperovich <imbaac@gmail.com>
2026-05-27 13:36:44 +0300

    chore: trim auth-starter

--- plugins/auth-starter/postgres/api/resources/users/endpoints/get-current.ts
+++
@@ -1,9 +0,0 @@
-import { isAuthorized } from '@/procedures';
-import { publicSchema } from '@/resources/users/users.schema';
-
-export default isAuthorized
-  .route({ method: 'GET', path: '/users/current' })
-  .output(publicSchema)
-  .handler(async ({ context }) => {
-  return context.user;
-});
--- plugins/auth-starter/web/services/auth-client.service.ts
+++
@@ -1,7 +0,0 @@
-import { createAuthClient } from 'better-auth/react';
-
-import config from 'config';
-
-export const authClient = createAuthClient({
-  baseURL: config.API_URL,
-});
